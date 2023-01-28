0204ee8e0b42afd0e6c215237e0caf55e26f8275
trininite <trininit3@gmail.com>
2023-01-28 06:47:59 -0500

    Creates folder + file with basic code

---
+++ newC_proj.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $#
+
+if [ $# -eq 1 ]; then
+	dir_name=$1
+
+	text="#include <stdio.h>\n\n\nint main() {\n\n    return 0;\n}"
+
+	mkdir $dir_name
+	file_name=$dir_name".c"
+
+	echo -e $text > $dir_name/$file_name
+	exit
+fi
+
+echo "Too many/few arguments, exiting..."
+
